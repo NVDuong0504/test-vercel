7e2d11e559558c79fda9e944935821a25c1d82e0
NVDuong0504 <nguoilahaui14@gmail.com>
2024-02-14 03:01:04 +0700

    up

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import express from 'express'
-import cors from 'cors'
-import env from '~/config/environment'
+// import cors from 'cors'
+// import env from '~/config/environment'
 
 import { CONNECT_DB } from '~/config/mongodb'
 // import { APIs_V1 } from '~/routes/v1'
-import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
+// import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER= () => {
   const app = express()
-  app.use(express.json())
-  app.use(cors())
+  // app.use(express.json())
+  // app.use(cors())
 
   // app.use('/v1', APIs_V1)
   app.get('/api',(req, res)=>{
@@ -17,10 +17,9 @@ const START_SERVER= () => {
   })
 
   //middleware xử lý lỗi tập trung
-  app.use(errorHandlingMiddleware)
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
+  // app.use(errorHandlingMiddleware)
+  app.listen(3000, () => {
     // eslint-disable-next-line no-console
-    console.log(`3. Hello ${env.AUTHOR}, I am running at ${ env.APP_PORT }:${  env.APP_HOST }`)
   })
 }
 
