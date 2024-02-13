17086a871cb481b370c1dcf5d2905fd9a61515b9
NVDuong0504 <nguoilahaui14@gmail.com>
2024-02-14 02:44:06 +0700

    u

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
-import express from "express";
+import express from 'express'
 
 const app = express()
 
-app.get('/api/home',(req, res)=>{
-  res.json({message:"message from home"})
+app.get('/api/home', (req, res) => {
+  res.json({message:'message from home'})
 })
 
-app.get("/api/about", (req, res)=>{
-  res.json({message:"message from about"})
+app.get('/api/about', (req, res) => {
+  res.json({message:'message from about'})
+})
+
+app.listen(5000, () => {
+  console.log(123)
 })
