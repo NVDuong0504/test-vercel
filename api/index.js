70f44bad9ed3b3cbcdba18de4d4e9148fb94e919
NVDuong0504 <nguoilahaui14@gmail.com>
2024-02-14 03:24:27 +0700

    update

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ import env from '../src/config/environment'
 
 import { CONNECT_DB } from '../src/config/mongodb.js'
 import { APIs_V1 } from '../src/routes/v1'
-import { errorHandlingMiddleware } from '../src/middlewares/errorHandlingMiddleware.js'
+// import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER= () => {
   const app = express()
-  app.use(express.json())
-  app.use(cors())
+  // app.use(express.json())
+  // app.use(cors())
 
-  app.use('/v1', APIs_V1)
+  // app.use('/v1', APIs_V1)
   app.get('/api',(req, res)=>{
     res.json({message: 13})
   })
