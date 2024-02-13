d5ad4a507d03aa67111d4e62b381ec3c33e3164c
NVDuong0504 <nguoilahaui14@gmail.com>
2024-02-14 05:33:50 +0700

    up

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const START_SERVER= () => {
   app.use(express.json())
   app.use(cors())
 
-  app.use('/v1', APIs_V1)
+  app.use('/api/v1', APIs_V1)
 
   //middleware xử lý lỗi tập trung
   app.use(errorHandlingMiddleware)
