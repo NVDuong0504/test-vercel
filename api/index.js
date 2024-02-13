b070a3ee544b20fdfb7ecb8b5f9aafba6c58e020
NVDuong0504 <nguoilahaui14@gmail.com>
2024-02-14 02:59:16 +0700

    u

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import env from '~/config/environment'
 
 import { CONNECT_DB } from '~/config/mongodb'
-import { APIs_V1 } from '~/routes/v1'
+// import { APIs_V1 } from '~/routes/v1'
 import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER= () => {
@@ -11,7 +11,10 @@ const START_SERVER= () => {
   app.use(express.json())
   app.use(cors())
 
-  app.use('/v1', APIs_V1)
+  // app.use('/v1', APIs_V1)
+  app.get('/api',(req, res)=>{
+    res.json({message: 13})
+  })
 
   //middleware xử lý lỗi tập trung
   app.use(errorHandlingMiddleware)
